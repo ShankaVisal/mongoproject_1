4a753c16d11f9880de6f1752b4f6ad15fed5693d
Shanka Visal <shankavisal@gmail.com>
2023-11-27 23:17:16 +0530

    Initial commit

---
+++ lib/dbHelper/constant.dart
@@ -0,0 +1,5 @@
+const MONGO_CONN_URL ='mongodb+srv://shankavisal:FhOJbdquqSXm7JZW@cluster0.mvsfcc1.mongodb.net/mydatabase?retryWrites=true&w=majority';
+const USER_COLLECTION ='youtube';
+
+// user = 'shankavisal'
+// password = 'FhOJbdquqSXm7JZW'
---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'dbHelper/mongodb.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await mongoDatabase.connect();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Homepage(),
+    );
+  }
+}
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+        child: Text('Hello'),
+      ),
+    );
+  }
+}
+
+
+
